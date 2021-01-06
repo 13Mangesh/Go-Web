c8f779e7953e120671bde98619b938e687d2c253
13Mangesh <13mangeshpuri@gmail.com>
2021-01-06 14:55:21 +0530

    Added Gorilla Mux and Styling

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"github.com/go-redis/redis"
+	"html/template"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "HellO World")
-}
+var client *redis.Client
+var templates *template.Template
+var ctx = context.Background()
 
 func main() {
-	http.HandleFunc("/", handler)
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+	
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
+
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	comments, err := client.LRange(ctx,"comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment := r.PostForm.Get("comment")
+	client.LPush(ctx, "comments", comment)
+	http.Redirect(w, r, "/", 302)
+}
+
